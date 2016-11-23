8a6f20c02f011b820fd06c0fd4fca02d364140bd
Wenjingjing <wj05040104@126.com>
2016-11-23 10:26:32 +0800

    00

--- start.sh
+++ start.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 rm test.log
+if [ -d ./errorScreenShot ]
+then
+	echo "errorScreenShot directory is exist !"
+else
+	mkdir errorScreenShot
+fi
 rm ./errorScreenShot/*.png
 python startTest.py | tee test.log
 
